69282e6395cf30d3e80568e0a1e06c5b87e0e59f
Egor Andreychuk <jegor.andreichuk@yandex.ru>
2023-04-21 19:00:42 +0300

    feat

--- controllers/cards.js
+++ controllers/cards.js
@@ -51,12 +51,6 @@ const likeCard = (req, res) => {
       { new: true, runValidators: true, upsert: true }
     )
     .orFail(new NotFoundError("Карточки с таким id не существует!"))
-    .then((card) => {
-      if (card) {
-        card.populate(['owner', 'likes'])
-      }
-      return card;
-    })
     .then((card) =>  {
       if(!card) {
         throw new NotFoundError();
@@ -77,12 +71,6 @@ const dislikeCard = (req, res) => {
       { new: true, runValidators: true, upsert: true }
     )
     .orFail(new NotFoundError("Карточки с таким id не существует!"))
-    .then((card) => {
-      if (card) {
-        card.populate(['owner', 'likes'])
-      }
-      return card;
-    })
     .then((card) =>  {
       if(!card) {
         throw new NotFoundError();
